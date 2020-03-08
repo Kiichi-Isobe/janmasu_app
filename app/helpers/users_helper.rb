1b86b155470b98add00f44466cddcb7d91f7bed5
Kiichi-Isobe <remingday@gmail.com>
2020-03-08 18:36:31 +0000

    ある平均順位をとる確率を計算する機能を追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,4 +14,11 @@ module UsersHelper
   def show_total_rank(user)
     "#{user.first_num}-#{user.second_num}-#{user.third_num}-#{user.fourth_num}"
   end
+
+  # 平均順位2.5のプレイヤーが与えられた平均順位をとる確率を返す
+  def cdf(user)
+    norm = Rubystats::NormalDistribution.new(2.5, (1.25 / user.game_results.size)**0.5)
+    cdf = norm.cdf(user.game_results.average(:rank))
+    (cdf * 100).round(1)
+  end
 end
